b6a7cd07f2d3b6e9cc28666d0477b861a40ea992
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2021-01-25 21:17:08 -0500

    added the styled component for side-menu

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,15 @@
 // import logo from './logo.svg'
-
+import React from 'react'
 import logo from '../logo.svg'
 import './App.css'
-
+import SideMenu from '../components/SideMenu'
 function App() {
-  return <div>Here we go</div>
+  return (
+    <React.Fragment>
+      <SideMenu></SideMenu>
+      <div>Here we go</div>
+    </React.Fragment>
+  )
 }
 
 export default App
