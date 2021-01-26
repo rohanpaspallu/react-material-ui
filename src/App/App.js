f4a53ab387f4ca3b53071987d6377878c84228ad
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2021-01-26 12:57:39 -0500

    header component added

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,26 @@ import React from 'react'
 import logo from '../logo.svg'
 import './App.css'
 import SideMenu from '../components/SideMenu'
+import { CssBaseline, makeStyles } from '@material-ui/core'
+import { Widgets } from '@material-ui/icons'
+import Header from '../components/Header'
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%',
+  },
+})
 function App() {
+  const classes = useStyles()
   return (
     <React.Fragment>
       <SideMenu></SideMenu>
-      <div>Here we go</div>
+
+      <div className={classes.appMain}>
+        <Header></Header>
+      </div>
+      <CssBaseline />
     </React.Fragment>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import {
+  AppBar,
+  Badge,
+  Grid,
+  IconButton,
+  InputBase,
+  Toolbar,
+} from '@material-ui/core'
+import React from 'react'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone'
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline'
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew'
+
+export default function Header() {
+  return (
+    <div>
+      <AppBar position='static'>
+        <Toolbar>
+          <Grid container>
+            <Grid item>
+              <InputBase />
+            </Grid>
+
+            <Grid item sm></Grid>
+            <Grid item>
+              <IconButton>
+                <Badge badgeContent={4} color='secondary'>
+                  <NotificationsNoneIcon></NotificationsNoneIcon>
+                </Badge>
+              </IconButton>
+              <IconButton>
+                <Badge badgeContent={3} color='primary'>
+                  <ChatBubbleOutlineIcon></ChatBubbleOutlineIcon>
+                </Badge>
+              </IconButton>
+
+              <IconButton>
+                <PowerSettingsNewIcon></PowerSettingsNewIcon>
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
