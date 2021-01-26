2d8287cd52fd836dc7f42b18d00eaebb61398071
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2021-01-25 21:59:01 -0500

    useStyles component implementation

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -2,33 +2,21 @@
 import React from 'react'
 import { makeStyles, withStyles } from '@material-ui/core'
 
-// const useStyles = makeStyles({
-//   sideMenu: {
-//     display: 'flex',
-//     flexDirection: 'row',
-//     position: 'absolute',
-//     left: '0px',
-//     width: '320px',
-//     height: '100%',
-//     backgroundColor: '#253053',
-//   },
-// })
-
-const useStyles = makeStyles({
+const style = {
   sideMenu: {
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
     position: 'absolute',
     left: '0px',
     width: '320px',
     height: '100%',
     backgroundColor: '#253053',
   },
-})
+}
 
-export default function SideMenu() {
-  const classes = useStyles()
+const SideMenu = (props) => {
+  const { classes } = props
   return <div className={classes.sideMenu}></div>
 }
 
-// export default SideMenu
+export default withStyles(style)(SideMenu)
