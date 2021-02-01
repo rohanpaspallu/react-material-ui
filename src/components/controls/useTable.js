d4266c49dde0d51b018d858da5ef35b0b1c87cd2
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2021-02-01 18:52:31 -0500

    added ascending and descending components to table

--- src/components/controls/useTable.js
+++ src/components/controls/useTable.js
@@ -5,6 +5,7 @@ import {
   TableHead,
   TablePagination,
   TableRow,
+  TableSortLabel,
 } from '@material-ui/core'
 import React, { useState } from 'react'
 
@@ -33,18 +34,39 @@ export default function useTable(records, headCells) {
   const pages = [1, 10, 25]
   const [page, setPage] = useState(0)
   const [rowsPerPage, setRowsPerPage] = useState(pages[page])
-  //   console.log(classes.table)
+  const [order, setOrder] = useState()
+  const [orderBy, setOrderBy] = useState()
 
   const TblContainer = (props) => (
     <Table className={classes.table}>{props.children}</Table>
   )
 
   const TblHead = (props) => {
+    const handleSortRequest = (cellId) => {
+      const isAsc = orderBy === cellId && order === 'asc'
+      setOrder(isAsc ? 'desc' : 'asc')
+      setOrderBy(cellId)
+    }
     return (
       <TableHead>
         <TableRow>
           {headCells.map((headCell) => {
-            return <TableCell key={headCell.id}>{headCell.label}</TableCell>
+            return (
+              <TableCell
+                key={headCell.id}
+                sortDirection={orderBy === headCell.id ? order : false}
+              >
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : 'asc'}
+                  onClick={() => {
+                    handleSortRequest(headCell.id)
+                  }}
+                >
+                  {headCell.label}
+                </TableSortLabel>
+              </TableCell>
+            )
           })}
         </TableRow>
       </TableHead>
@@ -73,8 +95,37 @@ export default function useTable(records, headCells) {
     )
   }
 
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index])
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0])
+      if (order !== 0) return order
+      return a[1] - b[1]
+    })
+    return stabilizedThis.map((el) => el[0])
+  }
+
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy)
+  }
+
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1
+    }
+    return 0
+  }
+
   const recordsAfterPagingAndSorting = () => {
-    return records.slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+    return stableSort(records, getComparator(order, orderBy)).slice(
+      page * rowsPerPage,
+      (page + 1) * rowsPerPage
+    )
   }
 
   return { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting }
